b9d900ba847c21934e6fc59b1c8deb9503ea0be2
kdg137 <208568416+zsh37@users.noreply.github.com>
2025-05-03 13:47:56 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,20 +3,20 @@ require("dotenv").config();
 
 module.exports = {
   bot: {
-    token: process.env.token || "MTMzODMyMTIyNDI3NDgwODkxMw.GEX1if.d5NVjkK5tXl-NPLYtGjUwhlCxPpxeVixvw9K3I", // Bot token
+    token: process.env.token || "", // Bot token
     clientName: process.env.clientName || "Mzika", // Bot username
-    clientId: process.env.clientId || "1338321224274808913", // Bot clientId
-    clientSecret: process.env.clientSecret || "GckZMv-E3WdEy662wchOHbstMibKxJa5", // Bot clientSecret
+    clientId: process.env.clientId || "", // Bot clientId
+    clientSecret: process.env.clientSecret || "", // Bot clientSecret
   },
 
   owner: {
-    userId: process.env.userId || "575172368524378113", // Owner userId for developer command
+    userId: process.env.userId || "", // Owner userId for developer command
     updateChecker: true, // Check for latest update. set to false if you dont want to check for latest update
   },
 
   botSettings: {
-    mongoUrl: process.env.mongoUrl || "mongodb+srv://mathewlueilwitz1337:Zabba.1337@cluster0.phb6g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", // Mongodb url for database
-    geniusToken: process.env.geniusToken || "sn39EljS0CYR1T_PYcx9P0dJCXVh5wzo0nxsComZgHbJaU5ph5LAsnf2sM-lhODa", // Genius token use to fetch lyrics, you can leave it blank.
+    mongoUrl: process.env.mongoUrl || "", // Mongodb url for database
+    geniusToken: process.env.geniusToken || "", // Genius token use to fetch lyrics, you can leave it blank.
     prefix: process.env.prefix || "!", // Default prefix is set to "?" use prefix command to change
     embedColor: process.env.embedColor || "2F3136", // You can use any HEX Color but without the "#"
   },
